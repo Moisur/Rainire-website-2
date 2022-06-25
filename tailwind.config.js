55e6cc4e46d91db5e554907349157446f2003faa
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-06-25 15:02:35 +0600

    header Compliyed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,31 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#ffffff",
+
+          "secondary": "#FF7594",
+
+          "accent": "#1FB2A6",
+
+          "neutral": "#2E1619",
+
+          "base-100": "#2A303C",
+
+          "info": "#ff7595",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
